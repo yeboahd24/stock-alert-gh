b9003cc1f0ec03c421fbe06da9eee6f71a1aae90
Dominic Kofi Yeboah <dominic.yeboah@bosonit.com>
2025-08-19 20:05:16 +0000

    posthog integration bug fixed api-key

--- src/main.tsx
+++ src/main.tsx
@@ -4,14 +4,26 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import { PostHogProvider } from 'posthog-js/react'
 
+const posthogKey = import.meta.env.VITE_PUBLIC_POSTHOG_KEY
+const posthogHost = import.meta.env.VITE_PUBLIC_POSTHOG_HOST
+
 const options = {
-  api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
+  api_host: posthogHost || 'https://eu.i.posthog.com',
+}
+
+const AppWithPostHog = () => {
+  if (posthogKey) {
+    return (
+      <PostHogProvider apiKey={posthogKey} options={options}>
+        <App />
+      </PostHogProvider>
+    )
+  }
+  return <App />
 }
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <PostHogProvider apiKey={import.meta.env.VITE_PUBLIC_POSTHOG_KEY} options={options}>
-      <App />
-    </PostHogProvider>
+    <AppWithPostHog />
   </StrictMode>,
-);
+)
